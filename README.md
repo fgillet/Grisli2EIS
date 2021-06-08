accebadbce46bd355fd630341d33d6be647b119d
fgillet <fabien.gillet-chaulet@univ-grenoble-alpes.fr>
2021-06-08 08:47:46 +0200

    start meshing tools

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 <!-- vim-markdown-toc GFM -->
 
 * [Introduction](#introduction)
+* [Contents](#contents)
 
 <!-- vim-markdown-toc -->
 
@@ -12,3 +13,9 @@
 
 Contains codes and configuration files to transfer model results from GRISLI
 [(the GRenoble Ice Sheet and Land Ice model)](https://gmd.copernicus.org/articles/11/5003/2018/) to [Elmer/Ice](http://elmerice.elmerfem.org/).
+
+## Contents
+
+* [Codes](./src): Usefull source codes
+* [MESH_0](./MESH_0): Create initial Elmer mesh from GRISLI ice-sheet mask
+
---
+++ src/MeshFunctions.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## Extract contour from GRISLI ice-sheet mask
+## Requires:
+##  * Input arguments:
+##    - $GRISLI: variable pointing to the GRISLI input file
+##    - $MaskVal: the number of the ice sheet (tacheg variable)
+##  * codes:
+##    - nco
+##    - gdal
+##
+ExtractPolygon () {
+ 
+ echo $MaskVal
+ echo $GRISLI
+
+ ## Maske binary mask where mask=1 for the required GRISLI mask value
+ ncap2 -v -s "mask=tacheg==$MaskVal" $GRISLI mask.nc
+
+ ## Get contour polygon from the mask
+ gdal_polygonize.py -mask NETCDF:"mask.nc":mask -f "ESRI Shapefile"  NETCDF:"mask.nc":mask contour.shp
+
+}
+
+
+## Convert the contour file to a gmsh .geo file 
+## (closed contour with unique lateral physical boundary)
+## Requires:
+##  * Input arguments:
+##    - $resolution: the required initial mesh resolution
+##    - $Contour2geo: python code in elmer sources to create .geo files
+##
+ShpToGeo () {
+  if [ ! -f "$Contour2geo" ]; then
+    echo "$Contour2geo does not exist. return ..."
+    exit 1
+  fi
+  python $Contour2geo -r $resolution -i contour.shp -o mesh.geo --spline
+}
+
+## Convert the gmsh .geo file to Elmer input format
+## Requires:
+##  * Codes:
+##   - gmsh
+##   - ElmerGrid
+##
+GeoToElmer () {
+  gmsh -1 -2 mesh.geo
+  ElmerGrid 14 2 mesh.msh -autoclean
+  ElmerGrid 2 5 mesh
+}
