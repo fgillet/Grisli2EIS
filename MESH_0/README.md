b9511d59efc61897733c38e017d0e0330bba7523
fgillet <fabien.gillet-chaulet@univ-grenoble-alpes.fr>
2021-06-08 12:36:05 +0200

    update mesh creation

--- MESH_0/README.md
+++ MESH_0/README.md
@@ -3,8 +3,28 @@
 ## Introduction
 
 
+
 ## Methods
 
+
+```bash
+export SRC_DIR=..
+source ${SRC_DIR}/src/MeshFunctions.sh
+```
+
 ![](../images//BarentsKara.png "Barents Kara Ice-Sheet mask and Contour")
+
+```bash
+GRISLI="${SRC_DIR}/DATA/LGM-IPSL-abs_euras20-pourElmer.nc"
+MaskVal=1
+
+ExtractPolygon
+```
+
+```bash
+Contour2geo=${ELMER_SRC}/elmerice/Meshers/GIS/Contour2geo.py
+resolution=500.0
+ShpToGeo
+```
 
 
--- src/MeshFunctions.sh
+++ src/MeshFunctions.sh
@@ -18,6 +18,7 @@ ExtractPolygon () {
  ncap2 -v -s "mask=tacheg==$MaskVal" $GRISLI mask.nc
 
  ## Get contour polygon from the mask
+ rm -rf contour.shp
  gdal_polygonize.py -mask NETCDF:"mask.nc":mask -f "ESRI Shapefile"  NETCDF:"mask.nc":mask contour.shp
 
 }
@@ -29,13 +30,13 @@ ExtractPolygon () {
 ##  * Input arguments:
 ##    - $resolution: the required initial mesh resolution
 ##    - $Contour2geo: python code in elmer sources to create .geo files
-##
+## Rq. --spline can induce loops and may fail on complex contours
 ShpToGeo () {
   if [ ! -f "$Contour2geo" ]; then
-    echo "$Contour2geo does not exist. return ..."
+    echo "Contour2geo does not exist. return ..."
     exit 1
   fi
-  python $Contour2geo -r $resolution -i contour.shp -o mesh.geo --spline
+  python $Contour2geo -r $resolution -i contour.shp -o mesh.geo 
 }
 
 ## Convert the gmsh .geo file to Elmer input format
